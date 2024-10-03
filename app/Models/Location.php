e7d29edc21ffb456e8efbeceac2e27832bef9fb6
Lauc1an <marlomp@hotmail.com>
2024-10-02 22:30:23 -0500

    Add Location Model, Seeder and Migration

---
+++ app/Models/Location.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Bagusindrayana\LaravelCoordinate\Traits\LaravelCoordinate;
+
+class Location extends Model
+{
+    use LaravelCoordinate;
+
+    public $_latitudeName = "map_latitude";
+    public $_longitudeName = "map_longitude";
+
+    protected $fillable = [
+        'name',
+        'phone',
+        'mobile',
+        'parking',
+        'country_id',
+        'city_id',
+        'district_id',
+        'address',
+        'map_latitude',
+        'map_longitude',
+        'active',
+        'company_id'
+    ];
+
+    protected $casts = [
+        'active' => 'boolean',
+    ];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function district()
+    {
+        return $this->belongsTo(District::class);
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+}
