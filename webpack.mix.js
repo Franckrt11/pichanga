fff60b26e906dc48974863f0a354633f0e2c4892
Lauc1an <marlomp@hotmail.com>
2024-10-14 22:20:32 -0500

    Add basic styles to web

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,8 @@
-const { copyFile } = require('fs');
 const mix = require('laravel-mix');
 
 mix.sass('resources/styles/admin/index.scss', 'public/css/admin.css')
     .sass('resources/styles/login/index.scss', 'public/css/login.css')
+    .sass('resources/styles/app/index.scss', 'public/css/app.css')
     .js('resources/scripts/login/index.js', 'public/js/login.js')
     .ts('resources/scripts/admin/index.ts', 'public/js/admin.js')
     .copyDirectory('resources/images', 'public/images')
