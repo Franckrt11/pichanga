dc138df4a25fd0965a7b599fdbffc68eb712f3e4
Lauc1an <marlomp@hotmail.com>
2024-10-14 22:20:47 -0500

    Add header to pages

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,2 +1,10 @@
-<header>
+<header class="header" role="banner">
+    <div class="header__container">
+        <div class="header__logo">
+            <a class="logo" href="{{ route('home') }}">
+                <img src="{{ asset('images/app/logo.svg') }}" alt="Logo">
+                <p>Te Juego una Pichanga</p>
+            </a>
+        </div>
+    </div>
 </header>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,15 @@
         <meta name="robots" content="noindex,nofollow">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        {{ $slot }}
+        <x-header />
+        <main class="main">
+            {{ $slot }}
+        </main>
     </body>
 </html>
